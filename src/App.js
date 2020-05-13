7dd22e784f69fa883e5b8b19019bc2519acbdfb4
younusraza909 <younusraza909@gmail.com>
2020-05-13 08:20:43 -0700

    Made Navbar Component using props and protype

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/layout/Navbar"
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-       <h1>Hello From React</h1>
+       <Navbar title="Github Finder" icon="fab fa-github"/>
       </div>
     );
   } 
