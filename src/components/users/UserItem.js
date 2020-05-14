fe0323f22f474b67a42c1f2c585e1ac69fa6f099
younusraza909 <younusraza909@gmail.com>
2020-05-14 08:12:10 -0700

    Setting enviroment variables

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const UserItem = ({ user: { avatar_url, html_url, login, id } }) => {
+const UserItem = ({ user: { avatar_url, html_url, login } }) => {
   return (
     <div className="card text-center">
       <img
