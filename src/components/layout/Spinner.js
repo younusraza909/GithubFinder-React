e5cde95bff44742db0d23bb4c08a342cbff0de0c
younusraza909 <younusraza909@gmail.com>
2020-05-14 06:43:10 -0700

    Spinner Component

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
+
+export const Spinner = () => {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        alt="loading ..."
+        style={{ width: "200px", display: "block", margin: "auto" }}
+      />
+    </Fragment>
+  );
+};
+
+export default Spinner;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map((user) => (
+        {users.map((user) => (
           <UserItem user={user} key={user.id} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
   gridGap: "1rem",
+};
+
+Users.propTypes = {
+  users: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default Users;
