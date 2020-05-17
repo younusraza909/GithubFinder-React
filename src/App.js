bd69618345d79f695fa65d45e7319e0e1742ef52
younusraza909 <younusraza909@gmail.com>
2020-05-17 08:11:37 -0700

    Page Not Found Component Made

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Fragment } from "react";
 import About from "./components/pages/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
+import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
+import NotFound from "./components/pages/NotFound";
+import Home from "./components/pages/Home";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -21,19 +21,11 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 {/* Here collon will pass login as parmater which we can get  */}
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
